92f546fcfeca2015c3fd0fbd5243323fe56ec1e7
Elton <eltoncasacio@hotmail.com>
2019-11-29 17:06:36 -0300

    instalado webpack, criado package.json....

---
+++ src/controller/WhatsAppController.js
@@ -0,0 +1,381 @@
+class WhatsAppController {
+
+    constructor() {
+
+        this.elementsPrototype();
+        this.loadElemtens();
+        this.initEvents();
+    }
+
+    initEvents() {
+
+        this.el.myPhoto.on('click', e => {
+
+            this.closeAllLeftPanel()
+            this.el.panelEditProfile.show();
+
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+            }, 50)
+
+        });
+
+        this.el.btnNewContact.on('click', e => {
+            this.closeAllLeftPanel()
+            this.el.panelAddContact.show();
+
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+            }, 50)
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+            this.el.panelEditProfile.removeClass('open');
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+            this.el.panelAddContact.removeClass('open');
+        });
+
+        this.el.photoContainerEditProfile.on('click', e => {
+
+            this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+
+            if (e.key === 'Enter') {
+
+                e.preventDefault();
+
+                this.el.btnSavePanelEditProfile.click();
+            }
+
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+            console.log(this.el.inputNamePanelEditProfile.innerHTML)
+        });
+
+        this.el.formPanelAddContact.on('submit', e => {
+
+            e.preventDefault();
+
+            let formData = new FormData(this)
+        });
+
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(contact => {
+
+            contact.on('click', e => {
+
+                this.el.main.css({
+                    display: 'flex'
+                });
+
+                this.el.home.hide();
+            });
+        });
+
+        this.el.btnAttach.on('click', e => {
+
+            e.stopPropagation()
+            this.el.menuAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+
+
+        });
+
+
+
+        this.el.btnAttachCamera.on('click', e => {
+
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height': 'calc(100% - 120px)'
+            });
+
+            this._camera = new CameraController(this.el.videoCamera);
+        });
+
+        this.el.btnClosePanelCamera.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+
+
+        this.el.btnAttachContact.on('click', e => {
+
+            this.el.modalContacts.show();
+        });
+
+        this.el.btnCloseModalContacts.on('click', e => {
+            this.el.modalContacts.hide();
+        });
+
+
+
+        this.el.btnAttachDocument.on('click', e => {
+
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                'height': 'calc(100% - 120px)'
+            });
+
+        });
+
+        this.el.btnSendDocument.on('click', e => {
+
+            console.log('enviando documento...')
+        })
+
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+
+
+        this.el.btnAttachPhoto.on('click', e => {
+
+            this.el.inputPhoto.click();
+        });
+
+        this.el.inputPhoto.on('change', e => {
+
+            console.log(this.el.inputPhoto.files);
+
+            [...this.el.inputPhoto.files].forEach(file => {
+
+                console.log(file)
+            });
+        });
+
+        this.el.btnTakePicture.on('click', e => {
+            console.log('tirando foto')
+        });
+
+
+        this.el.btnSendMicrophone.on('click', e => {
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.startRecordMicrophoneTime();
+        });
+
+        this.el.btnCancelMicrophone.on('click', e => {
+
+            this.closeRecorMicrophone();
+
+        });
+
+        this.el.btnFinishMicrophone.on('click', e => {
+
+            this.closeRecorMicrophone();
+        });
+
+
+        this.el.inputText.on('keypress', e => {
+
+            if (e.key === 'Enter' && !e.ctrlKey) {
+
+                e.preventDefault();
+                this.el.btnSend.click();
+            }
+        });
+
+        this.el.inputText.on('keyup', e => {
+
+            if (this.el.inputText.innerHTML.length) {
+
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+
+            } else {
+
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+            }
+        });
+
+        this.el.btnSend.on('click', e => {
+
+            console.log('enviando mensagem')
+        });
+
+        this.el.btnEmojis.on('click', e => {
+
+            this.el.panelEmojis.toggleClass('open');
+        });
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
+
+            emoji.on('click', e => {
+
+                //   = emoji.dataset.unicode
+                let img = this.el.imgEmojiDefault.cloneNode();
+
+                img.style.cssText = emoji.style.cssText;
+                img.dataset.unicode = emoji.dataset.unicode;
+                img.alt = emoji.dataset.unicode;
+
+                emoji.classList.forEach(name => {
+                    img.classList.add(name)
+                });
+
+                this.el.inputPlaceholder.hide();
+                this.el.btnSend.show();
+
+                let cursor = window.getSelection();
+
+                if (!cursor.focusNode || !cursor.focusNode.id == 'input-text') {
+                    this.el.inputText.focus();
+
+                    cursor = window.getSelection();
+                }
+
+                let range = document.createRange();
+
+                range = cursor.getRangeAt(0);
+                range.deleteContents();
+
+                let frag = document.createDocumentFragment();
+
+                frag.appendChild(img);
+
+                range.insertNode(frag);
+
+                range.setStartAfter(img);
+
+            });
+        });
+
+
+
+    }
+
+    startRecordMicrophoneTime() {
+        let start = Date.now();
+
+        this._recordMicrophoneInterval = setInterval(() => {
+
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
+        }, 100)
+    }
+
+    closeRecorMicrophone() {
+
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+
+        clearInterval(this._recordMicrophoneInterval);
+    }
+
+    closeAllMainPanel() {
+
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
+    }
+
+
+    closeMenuAttach(e) {
+
+        document.removeEventListener('click', this.closeMenuAttach);
+        this.el.menuAttach.removeClass('open');
+
+    }
+
+    closeAllLeftPanel() {
+        this.el.panelEditProfile.hide()
+        this.el.panelAddContact.hide()
+    }
+
+    loadElemtens() {
+
+
+        this.el = {};
+
+        document.querySelectorAll('[id]').forEach(element => {
+
+            this.el[Format.getCamelCase(element.id)] = element
+        });
+
+    }
+
+    elementsPrototype() {
+
+        Element.prototype.hide = function () {
+            this.style.display = 'none';
+            return this
+        }
+
+        Element.prototype.show = function () {
+            this.style.display = 'block';
+            return this
+        }
+
+        Element.prototype.toggle = function () {
+            this.style.display = (this.style.display === 'block') ? 'none' : 'block';
+            return this
+        }
+
+        Element.prototype.on = function (events, fn) {
+
+            events.split(' ').forEach(event => {
+
+                this.addEventListener(event, fn);
+            });
+            return this
+        }
+
+        Element.prototype.css = function (styles) {
+
+            for (let name in styles) {
+                this.style[name] = styles[name]
+            }
+            return this
+        }
+
+        Element.prototype.addClass = function (className) {
+            this.classList.add(className)
+            return this
+        }
+
+        Element.prototype.removeClass = function (className) {
+            this.classList.remove(className)
+            return this
+        }
+
+        Element.prototype.toggleClass = function (className) {
+            this.classList.toggle(className)
+            return this
+        }
+
+        Element.prototype.hasClass = function (className) {
+            return this.classList.contains(className)
+        }
+
+        HTMLElement.prototype.getForm = function () {
+            return new FormData(this)
+        }
+
+        HTMLElement.prototype.toJSON = function () {
+
+            let json = {};
+
+            this.getForm().forEach((value, key) => {
+                json[key] = value
+            });
+
+            return json
+        }
+
+
+    }
+
+}
---
+++ src/util/Format.js
@@ -0,0 +1,27 @@
+class Format {
+
+    static getCamelCase(elementId) {
+
+        let div = document.createElement('div');
+
+        div.innerHTML = `<div data-${elementId}="id"></div>`;
+
+        return Object.keys(div.firstChild.dataset)[0]
+
+    }
+
+    static toTime(duration) {
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        if (hours) {
+            return `${hours}: ${minutes.toString().padStart(2, '0')}: ${seconds.toString().padStart(2, '0')}`;
+        }
+        else {
+            return `${minutes}: ${seconds.toString().padStart(2, '0')}`;
+
+        }
+    }
+
+}
